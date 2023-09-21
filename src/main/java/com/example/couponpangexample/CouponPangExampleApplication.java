762c27600f668b37ac9cebc82c57036d512a795b
유형우/애버커스 <db6194@iabacus.co.kr>
2023-09-21 11:07:10 +0900

    init

---
+++ src/main/java/com/example/couponpangexample/CouponPangExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.couponpangexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponPangExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponPangExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/couponpangexample/CouponPangExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.couponpangexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponPangExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
